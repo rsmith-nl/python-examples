e2d248281cbbea94741a175c5c8e5ba0a22109a7
Roland Smith <rsmith@xs4all.nl>
2022-02-06 14:06:30 +0100

    Apply reformat with black.

--- multiprocessing/eventtest.py
+++ multiprocessing/eventtest.py
@@ -16,30 +16,31 @@ import time
 
 def worker(num, sw, sm):
     if num == 5:
-        print('This is worker', num)
+        print("This is worker", num)
         time.sleep(1)
-        print('Worker', num, 'signalling main program to quit')
+        print("Worker", num, "signalling main program to quit")
         sm.set()
     while not sw.is_set():
-        print('This is worker', num)
+        print("This is worker", num)
         time.sleep(0.7)
     else:
-        print('Worker', num, 'signing off..')
+        print("Worker", num, "signing off..")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     stop_worker = mp.Event()
     stop_main = mp.Event()
 
-    workers = [mp.Process(target=worker, args=(n, stop_worker, stop_main))
-               for n in range(1, 6)]
+    workers = [
+        mp.Process(target=worker, args=(n, stop_worker, stop_main)) for n in range(1, 6)
+    ]
     for w in workers:
         w.start()
     while not stop_main.is_set():
         time.sleep(1)
-    print('MAIN: Received stop event')
-    print('MAIN: Sending stop event to workers')
+    print("MAIN: Received stop event")
+    print("MAIN: Sending stop event to workers")
     stop_worker.set()
     for c, w in enumerate(workers, start=1):
         w.join()
-        print('worker', c, 'joined')
+        print("worker", c, "joined")
--- vigenère_cipher.py
+++ vigenère_cipher.py
@@ -11,22 +11,22 @@ Vigenère encryption.
 Adapted from my answer to https://stackoverflow.com/questions/36780703.
 """
 
-plaintext = 'THISISAPLAINTEXT'
+plaintext = "THISISAPLAINTEXT"
 print(f"plaintext = {plaintext}")
-key = 'SPAMEGGS'
+key = "SPAMEGGS"
 print(f"key = {key}")
-count = int(len(plaintext)/len(key))+1
+count = int(len(plaintext) / len(key)) + 1
 
-stretchedkey = [ord(c) for c in key*count]
+stretchedkey = [ord(c) for c in key * count]
 
 # Encryption
 plainnum = [ord(c) for c in plaintext]
-ciphernum = [a+b-65 for a, b in zip(plainnum, stretchedkey)]
-ciphertext = ''.join([chr(c) if c <= 90 else chr(c-26) for c in ciphernum])
+ciphernum = [a + b - 65 for a, b in zip(plainnum, stretchedkey)]
+ciphertext = "".join([chr(c) if c <= 90 else chr(c - 26) for c in ciphernum])
 print(f"ciphertext = {ciphertext}")
 
 # Decryption
 ciphernum = [ord(c) for c in ciphertext]
-decryptnum = [a-b+65 for a, b in zip(ciphernum, stretchedkey)]
-decrypted = ''.join([chr(c) if c >= 65 else chr(c+26) for c in decryptnum])
+decryptnum = [a - b + 65 for a, b in zip(ciphernum, stretchedkey)]
+decrypted = "".join([chr(c) if c >= 65 else chr(c + 26) for c in decryptnum])
 print(f"decrypted = {decrypted}")
